dc2bda0529d5febc82f5f68cc62951e223d8142a
Aleksandar Geyman <aleksandar.geyman@upnetix.com>
2020-05-18 09:08:22 +0300

    initial commit

---
+++ RandomatorDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomatorDemoApp
+//
+//  Created by Aleksandar Geyman on 18.05.20.
+//  Copyright © 2020 Aleksandar Geyman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
